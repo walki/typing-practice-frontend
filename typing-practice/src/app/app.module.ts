77a593aa9e098de1f8135c5ecd9a1587ccab1293
Roger Walker <roger.john.walker@gmail.com>
2020-05-10 17:15:44 -0400

    Getting ng-bootstrap set up, textare for typing

--- typing-practice/src/app/app.module.ts
+++ typing-practice/src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { TypingFieldComponent } from './typing-field/typing-field.component';
 import { CurrentStatsComponent } from './current-stats/current-stats.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -16,7 +18,9 @@ import { CurrentStatsComponent } from './current-stats/current-stats.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
